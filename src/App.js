6053a30e017377923cd3fbe1e0a0f33844749602
anhtt <anhttb01535@gmail.com>
2025-08-26 16:44:03 +0000

    update code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,143 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { useEffect, useState} from 'react';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // use state
+  const [totalPage, setTotalPage] = useState(1);
+
+  const handleSuggestedClick = (event) => {
+    const td = event.target.closest("td"); // get closest <td>
+    const answerDiv = td.querySelector(".answer");
+    if (answerDiv) {
+      answerDiv.style.display = answerDiv.style.display === "none" ? "block" : "none";
+    }
+    event.target.value = answerDiv.style.display === "block" ? "Hide Suggested Answer" : "Show Suggested Answer";
+  };
+
+  const handleVotingClick = (event) => {
+    const td = event.target.closest("td"); // get closest <td>
+    const votingDiv = td.querySelector(".voting-answer");
+    if (votingDiv) {
+      votingDiv.style.display = votingDiv.style.display === "none" ? "block" : "none";
+    }
+    event.target.value = votingDiv.style.display === "block" ? "Hide Voting Answer" : "Show Voting Answer";
+  };
+
+  const handlePreviousPageClick = (event) => {
+    const newPage = currentPage - 1;
+    setCurrentPage(newPage); // update state
+    fetch(`/api/questions?page=${newPage}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data.data);
+        setTotalPage(data.totalPage);
+      })
+      .catch((error) => console.error('Error fetching questions:', error));
+  };
+
+  const handleNextPageClick = (event) => {
+    const newPage = currentPage + 1;
+    setCurrentPage(newPage); // update state
+    fetch(`/api/questions?page=${newPage}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data.data);
+        setTotalPage(data.totalPage);
+      })
+      .catch((error) => console.error('Error fetching questions:', error));
+  };
+
+  const handlePageSelect = (event) => {
+    const selectedPage = Number(event.target.value);
+    setCurrentPage(selectedPage);
+    fetch(`/api/questions?page=${selectedPage}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data.data);
+        setTotalPage(data.totalPage);
+      })
+      .catch((error) => console.error('Error fetching questions:', error));
+  };
 
   useEffect(() => {
-    fetch('/api/hello')
+    fetch(`/api/questions?page=1`)
       .then((response) => response.json())
-      .then((data) => setData(data.message));
+      .then((data) => {
+        setQuestions(data.data);
+        setTotalPage(data.totalPage);
+      })
+      .catch((error) => console.error('Error fetching questions:', error));
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React test
-        </a>
-      </header>
       <div>
-        <h1>Welcome to My React App</h1>
-        <p>This is a simple React application.</p>
-        <p>data from server: {data}</p>
+        <div style={{ position: 'sticky' , top: '0px', backgroundColor: '#95acc2ff', padding: '10px', zIndex: '1' }}>
+          <h1 style={{ marginTop: '0px', marginBottom: '10px', color: 'white'}}>Welcome to Exam question</h1>
+          <input type='button' value='previous page' disabled={currentPage === 1} onClick={handlePreviousPageClick} style={{ marginRight: '10px' }}/>
+          <input type='button' value='next page' disabled={questions.length < 20} onClick={handleNextPageClick} style={{ marginRight: '10px' }}/>
+          <select onChange={handlePageSelect}>
+            {Array.from({ length: totalPage }, (_, i) => i + 1).map(pageNum => (
+              <option key={pageNum} value={pageNum} selected={pageNum === currentPage}>{pageNum}</option>
+            ))}
+          </select>
+        </div>
+        <table style={{ textAlign: 'left' , marginLeft: '20px'}}>
+          <tbody>
+            {questions.map(q => (
+              <tr key={q.id}>
+                <td>
+                  <p style={{ margin: '0px'}}>
+                    <strong>{q.questionNo}. {" "}</strong>
+                    {q.content.split("\n").map((line, index) => (
+                      <React.Fragment key={index}>
+                        {line}
+                        <br />
+                      </React.Fragment>
+                    ))}
+                  </p>
+                  <br/>
+                  <div>
+                    {q.questionImages != null && q.questionImages.map(img => (
+                      // <img key={img.id} src={`data:image/png;base64,${img.imageBase64}`} alt={`Image ${img.id}`} style={{ width: "50%", maxWidth: "900px", margin: "5px" }}/>
+                      <img key={img.id} src={img.imageUrl} alt={`Image ${img.id}`} style={{ width: "50%", maxWidth: "900px", margin: "5px" }}/>
+                    ))}
+                  </div>
+                  {q.questionChoices.map( c => (
+                    <div key={c.id}>
+                      <label>{c.content}</label>
+                    </div>
+                  ))}
+                  <input type='button' className='show-answer' value='Show Suggested Answer' style={{ marginTop: '10px', marginRight: '10px' }} onClick={handleSuggestedClick}/>
+                  <input type='button' className='show-voting' value='Show Voting Answer' style={{ marginTop: '10px' }} onClick={handleVotingClick}/>
+                  <div className='answer' style={{ display: 'none' }}>
+                    <strong>Suggested answer: </strong>{q.answer.content}
+                  </div>
+                  <div className='voting-answer' style={{ display: 'none' }}>
+                    <strong>Voting answer:</strong>
+                    {q.votingAnswers.map( v => (
+                      <div key={v.id}>
+                        <label>{v.content}: {v.rating}%</label>
+                      </div>
+                    ))}
+                  </div>
+                  <br />
+                  <a href={q.sourceUrl} target="_blank" rel="noopener noreferrer">{q.sourceUrl}</a>
+                  <hr />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <input type='button' value='previous page' disabled={currentPage === 1} onClick={handlePreviousPageClick} style={{ marginRight: '10px' }}/>
+        <input type='button' value='next page' disabled={questions.length < 20} onClick={handleNextPageClick} style={{ marginRight: '10px' }}/>
+        <select onChange={handlePageSelect}>
+          {Array.from({ length: totalPage }, (_, i) => i + 1).map(pageNum => (
+            <option key={pageNum} value={pageNum} selected={pageNum === currentPage}>{pageNum}</option>
+          ))}
+        </select>
       </div>
     </div>
   );
